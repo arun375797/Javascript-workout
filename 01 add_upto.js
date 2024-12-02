c7bca56d67e63b398fc9691b65a2799bac2da194
arun375797 <arun375797@gmail.com>
2024-12-02 14:34:37 +0530

    array reverse

---
+++ 01 add_upto.js
@@ -0,0 +1,5 @@
+const addUpTo=function(n){
+    return n*(n+1)/2
+}
+
+console.log(addUpTo(3))
---
+++ 03 array_reverse.js
@@ -0,0 +1,4 @@
+const array=[1,2,3,4,5,6,7]
+const array2=[1,2,3,8,5,6,0]
+console.log(array.reverse())
+console.log(array2.reverse())
